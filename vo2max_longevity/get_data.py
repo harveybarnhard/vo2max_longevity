bf968cf3014b51d2c2409855925f1da0e62e9a8c
Harvey Barnhard <40671793+harveybarnhard@users.noreply.github.com>
2023-04-22 10:18:42 -0400

    Fixing import statement

--- vo2max_longevity/get_data.py
+++ vo2max_longevity/get_data.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.options import Service
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
 
